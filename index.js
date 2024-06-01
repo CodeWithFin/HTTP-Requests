5ebaa6c885c94f6d79948875bb05c10b8feff4c8
Finley <finley.mwachia12@gmail.com>
2024-06-01 09:08:16 +0300

    added code to js file

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/",(req,res)=>{
+    res.send("Hello world!");
+})
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+})
